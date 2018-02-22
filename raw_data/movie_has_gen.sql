e14f60f177f5b1b3508ea4956d03d4eec84218d8
Siddhant Kushwaha <siddhant.k16@iiits.in>
2018-02-23 02:42:46 +0530

    sorry

--- raw_data/movie_has_gen.sql
+++ raw_data/movie_has_gen.sql
@@ -1,9 +1,9 @@
 use movie_database;
-
+drop table movie_has_genres;
 CREATE TABLE movie_has_genres(
-    gid int,
     mid int,
+    gid int,
     PRIMARY KEY(mid, gid)
 );
 
-load data local infile "/Users/siddhantkushwaha/Desktop/movie_has_gen.txt" into table movie_has_genres fields terminated by ' ';
+load data local infile "/Users/siddhantkushwaha/Desktop/work/dbms/raw_data/movie_has_gen.txt" into table movie_has_genres fields terminated by ' ';
